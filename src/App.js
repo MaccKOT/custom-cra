3f80f42bd997cc0daba1c99cfc0f188141facf91
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-08-15 13:26:04 +0400

    All done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css'; //some of basic css
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default App;
